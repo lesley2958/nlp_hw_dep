ec71d6d24c9f92c3e269b160221540bd4977f429
lc2958@columbia.edu <lc2958@columbia.edu>
2018-01-14 06:46:01 -0500

    update

---
+++ depModel.py
@@ -0,0 +1,61 @@
+import os,sys
+from decoder import *
+
+class DepModel:
+    def __init__(self):
+        '''
+            You can add more arguments for examples actions and model paths.
+            You need to load your model here.
+            actions: provides indices for actions.
+            it has the same order as the data/vocabs.actions file.
+        '''
+        # if you prefer to have your own index for actions, change this.
+        self.actions = ['SHIFT', 'LEFT-ARC:rroot', 'LEFT-ARC:cc', 'LEFT-ARC:number', 'LEFT-ARC:ccomp', 'LEFT-ARC:possessive', 'LEFT-ARC:prt', 'LEFT-ARC:num', 'LEFT-ARC:nsubjpass', 'LEFT-ARC:csubj', 'LEFT-ARC:conj', 'LEFT-ARC:dobj', 'LEFT-ARC:nn', 'LEFT-ARC:neg', 'LEFT-ARC:discourse', 'LEFT-ARC:mark', 'LEFT-ARC:auxpass', 'LEFT-ARC:infmod', 'LEFT-ARC:mwe', 'LEFT-ARC:advcl', 'LEFT-ARC:aux', 'LEFT-ARC:prep', 'LEFT-ARC:parataxis', 'LEFT-ARC:nsubj', 'LEFT-ARC:<null>', 'LEFT-ARC:rcmod', 'LEFT-ARC:advmod', 'LEFT-ARC:punct', 'LEFT-ARC:quantmod', 'LEFT-ARC:tmod', 'LEFT-ARC:acomp', 'LEFT-ARC:pcomp', 'LEFT-ARC:poss', 'LEFT-ARC:npadvmod', 'LEFT-ARC:xcomp', 'LEFT-ARC:cop', 'LEFT-ARC:partmod', 'LEFT-ARC:dep', 'LEFT-ARC:appos', 'LEFT-ARC:det', 'LEFT-ARC:amod', 'LEFT-ARC:pobj', 'LEFT-ARC:iobj', 'LEFT-ARC:expl', 'LEFT-ARC:predet', 'LEFT-ARC:preconj', 'LEFT-ARC:root', 'RIGHT-ARC:rroot', 'RIGHT-ARC:cc', 'RIGHT-ARC:number', 'RIGHT-ARC:ccomp', 'RIGHT-ARC:possessive', 'RIGHT-ARC:prt', 'RIGHT-ARC:num', 'RIGHT-ARC:nsubjpass', 'RIGHT-ARC:csubj', 'RIGHT-ARC:conj', 'RIGHT-ARC:dobj', 'RIGHT-ARC:nn', 'RIGHT-ARC:neg', 'RIGHT-ARC:discourse', 'RIGHT-ARC:mark', 'RIGHT-ARC:auxpass', 'RIGHT-ARC:infmod', 'RIGHT-ARC:mwe', 'RIGHT-ARC:advcl', 'RIGHT-ARC:aux', 'RIGHT-ARC:prep', 'RIGHT-ARC:parataxis', 'RIGHT-ARC:nsubj', 'RIGHT-ARC:<null>', 'RIGHT-ARC:rcmod', 'RIGHT-ARC:advmod', 'RIGHT-ARC:punct', 'RIGHT-ARC:quantmod', 'RIGHT-ARC:tmod', 'RIGHT-ARC:acomp', 'RIGHT-ARC:pcomp', 'RIGHT-ARC:poss', 'RIGHT-ARC:npadvmod', 'RIGHT-ARC:xcomp', 'RIGHT-ARC:cop', 'RIGHT-ARC:partmod', 'RIGHT-ARC:dep', 'RIGHT-ARC:appos', 'RIGHT-ARC:det', 'RIGHT-ARC:amod', 'RIGHT-ARC:pobj', 'RIGHT-ARC:iobj', 'RIGHT-ARC:expl', 'RIGHT-ARC:predet', 'RIGHT-ARC:preconj', 'RIGHT-ARC:root']
+        # write your code here for additional parameters.
+        self.word_ft = []
+        self.pos_ft = []
+        self.labels_ft = []
+        
+        with open('./data/vocabs.word') as f1: 
+            for i in f1:
+                self.word_ft.append(i.split()[0])
+        with open('./data/vocabs.pos') as f1: 
+            for i in f1:
+                self.pos_ft.append(i.split()[0])
+        with open('./data/vocabs.labels') as f1: 
+            for i in f1:
+                self.labels_ft.append(i.split()[0])
+        # feel free to add more arguments to the initializer.
+
+    def score(self, str_features):
+        '''
+        :param str_features: String features
+        20 first: words, next 20: pos, next 12: dependency labels.
+        DO NOT ADD ANY ARGUMENTS TO THIS FUNCTION.
+        :return: list of scores
+        '''
+        # add for when the word/pos/label isnt there
+        scores = []
+        for i in str_features[:20]: 
+            try: 
+                scores.append(self.word_ft.index(i))
+            except:
+                scores.append(0)
+        for i in str_features[20:40]:
+            try:
+                scores.append(self.pos_ft.index(i))
+            except:
+                scores.append(0)
+        for i in str_features[40:]:
+            try:
+                scores.append(self.labels_ft.index(i))
+            except:
+                scores.append(0)
+        # change this part of the code.
+        return scores
+
+if __name__=='__main__':
+    m = DepModel()
+    input_p = os.path.abspath(sys.argv[1])
+    output_p = os.path.abspath(sys.argv[2])
+    Decoder(m.score, m.actions).parse(input_p, output_p)
